996d9c333eb76c3f7bcc6122deb1b42b08af291f
Samuel Oliveira <samuelmaia1020@gmail.com>
2022-12-03 22:40:10 -0300

    alteração no codigo

---
+++ aula10/comentario.js
@@ -0,0 +1,16 @@
+/* 
+* Aritméticos
+* + = adição 
+* ** = potenciação 
+* % = resto da divisão
+* * = multiplição
+* / = divisão
+*/ 
+const num1 = 10;
+const num2 = 5;
+const num3 = 2;
+console.log(num1 + num2);
+console.log(num1 ** num2);
+console.log(num1 / num3);
+console.log(num1 + num2 * num3);
+console.log((num1 * num2) / num3);
--- aula9/index.js
+++ aula9/index.js
@@ -1,1 +1,10 @@
-console.log('teste');
+const nome = 'samuel' // string
+const nome2 = 'oliveira' // string
+const number = 18 // numero
+const number2 = 18.50 // numero
+let nomealuno; //undefined > não aponta para local nenhum na memória // 
+const sobrenome = null; // nulo >  não aponta para local nenhum na memória //
+const aprovado = falso; // boolean = true ou false (lógio) //
+
+let a = 2;
+const b = a;
