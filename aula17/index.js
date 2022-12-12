138a92e475f36ebf5c115014b3f54f9b29a42931
Samuel Oliveira <samuelmaia1020@gmail.com>
2022-12-12 12:36:26 -0300

    adicionar novos arquivos

---
+++ aula17/index.js
@@ -0,0 +1,20 @@
+/* function saudacao(nome){
+    console.log(`Bom dia ${nome}`);
+}
+
+saudacao('Samuel'); */
+
+/* function soma(x,y){
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(2, 5)); */
+
+/* const raiz = function (n){
+    return n ** 0.5;
+}
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25)); */
